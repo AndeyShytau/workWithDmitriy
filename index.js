34ddb8c54158e963eee6bf3e03bf95427cd4810f
AndeyShytau <seb7742@yandex.ru>
2023-07-07 20:26:44 +0300

    add start projekt

---
+++ index.js
@@ -0,0 +1,15 @@
+// var userName = 'Vasiliy';
+// var message = 'Hello';
+// console.log(userName);
+
+// showMessage = () => {
+// alert(message+' '+userName)
+// };
+// showMessage();
+
+// var styles = ["Джаз", "Блюз"];
+// styles[2] = "Рок-н-ролл";
+// styles[1] = "Классика";
+// alert(styles.shift());
+// styles.unshift("Реп", "Регги");
+// console.log(styles);

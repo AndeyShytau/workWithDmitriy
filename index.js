26b59ff3eecc36bb2dc7a856709d7eb9575b8a6c
AndeyShytau <seb7742@yandex.ru>
2023-07-07 21:03:38 +0300

    add new in js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// var userName = 'Vasiliy';
-// var message = 'Hello';
-// console.log(userName);
+var userName = 'Vasiliy';
+var message = 'Hello';
+console.log(userName);
 
-// showMessage = () => {
-// alert(message+' '+userName)
-// };
-// showMessage();
+showMessage = () => {
+alert(message+' '+userName)
+};
+showMessage();
 
-// var styles = ["Джаз", "Блюз"];
-// styles[2] = "Рок-н-ролл";
-// styles[1] = "Классика";
-// alert(styles.shift());
-// styles.unshift("Реп", "Регги");
-// console.log(styles);
+var styles = ["Джаз", "Блюз"];
+styles[2] = "Рок-н-ролл";
+styles[1] = "Классика";
+alert(styles.shift());
+styles.unshift("Реп", "Регги");
+console.log(styles);
+
+

b5ba4826abcf23ecd2e5ab891b7f89126c95a802
AndeyShytau <seb7742@yandex.ru>
2023-07-13 10:11:18 +0300

    add js task 1-3 ?but i cant make 4-5

--- index.js
+++ index.js
@@ -1,23 +1,68 @@
-/*var userName = 'Vasiliy';
-var message = 'Hello';
-console.log(userName);
+// var userName = 'Vasiliy';
+// var message = 'Hello';
+// console.log(userName);
+//
+// showMessage = () => {
+//     alert(message + ' ' + userName)
+// };
+// showMessage();
 
-showMessage = () => {
-alert(message+' '+userName)
+// var styles = ["Джаз", "Блюз"];
+// styles[2] = "Рок-н-ролл";
+// styles[1] = "Классика";
+// alert(styles.shift());
+// styles.unshift("Реп", "Регги");
+// console.log(styles);
+
+let currentUser = {
+    id: 1,
+    gender: 'male',
+    age: 33,
+    name: 'andrey',
+    surname: 'shytau',
+    surname1: 'valerevich',
+    brif: '',
+    status: 'unmarred',
+    city: 'minsk',
+    phone: '+375293573759',
+    telegramm: '@Andrey_Shutoff',
+    instagramm: '',
+    twitter: '',
+    classmates: [],
+    friends: [],
+    subscribers: [],
 };
-showMessage();
 
-var styles = ["Джаз", "Блюз"];
-styles[2] = "Рок-н-ролл";
-styles[1] = "Классика";
-alert(styles.shift());
-styles.unshift("Реп", "Регги");
-console.log(styles);*/
+let userNews = [{
+    text: 'news 1',
+    picture: '',
+    creationDate: '',
+    comments: ['', '']
+}, {
+    text: 'news 2',
+    picture: '',
+    creationDate: '',
+    comments: ['', '']
+}];
 
-const logoElem = document.getElementById("someButton");
-console.log(logoElem);
-const alertFunc = () => {
-    console.log("sdvsd")
-}
-logoElem.onclick = alertFunc;
+let userMessages = [{
+    senderName: '',
+    senderID: 22,
+    senderPhoto: '',
+    text: '',
+    sendDate: '',
+}, {
+    senderName: '',
+    senderID: 23,
+    senderPhoto: '',
+    text: '',
+    sendDate: '',
+}];
 
+let el1 = document.getElementsByClassName('basicContent');
+let el2 = document.getElementsByClassName('moreProfileInfoButton');
+let el3 = document.getElementsByClassName('newsSection');
+
+el2.addEventListener("click", function() {
+    alert("hhhhhhhhhh");
+});

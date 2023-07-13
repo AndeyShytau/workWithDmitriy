79989fec4369b7a718812ce81d5efbe1d0bee5b5
AndeyShytau <seb7742@yandex.ru>
2023-07-13 13:55:10 +0300

    add js task 4-5

--- index.js
+++ index.js
@@ -59,10 +59,30 @@ let userMessages = [{
     sendDate: '',
 }];
 
-let el1 = document.getElementsByClassName('basicContent');
-let el2 = document.getElementsByClassName('moreProfileInfoButton');
+let el1 = document.getElementById('contentContainer');
+let el2 = document.getElementById('moreInfoButton');
 let el3 = document.getElementsByClassName('newsSection');
-
-el2.addEventListener("click", function() {
-    alert("hhhhhhhhhh");
+let el4 = document.getElementById('moreInfoProfile');
+let el5 = document.getElementById('news1');
+let el6 = document.getElementById('sendNewsButton');
+el2.addEventListener("click", function () {
+    if (el4.style.height = '0px') {
+        el4.style.height = '200px';
+        el2.textContent = 'Cвернуть';
+    } else {
+        el4.style.height = '0px';
+        el2.textContent = 'Показать больше';
+    }
 });
+
+el6.addEventListener("click", function () {
+    let infoToArr = el6.textContent;
+    console.log(infoToArr);
+    console.log("hhhhhhhhhh");
+})
+
+// el2.removeEventListener("click", function () {
+//     console.log("hhhhhhhhhh");
+//     el4.style.height = '200px';
+//     el2.textContent = 'свернуть';
+// });

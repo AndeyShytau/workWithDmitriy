fa422ee13a401b68536b54922280ca907d88f476
d-vooolk <volk.dmitrii@otr.ru>
2023-08-03 00:01:48 +0300

    add redirect to home page if auth is successfully

--- pages/auth/script.js
+++ pages/auth/script.js
@@ -9,12 +9,28 @@ const authData = {
     password: null,
 }
 
+const redirectPath = window.location.href.split('/').reduce((acc, item) => {
+    if (item === 'pages') {
+        acc.toggleFinderElement = false;
+    }
+    if (acc.toggleFinderElement) {
+        acc.pathSegments.push(item);
+    }
+    return acc;
+}, { toggleFinderElement: true, pathSegments: [] }).pathSegments.join('/');
+
 authButton.onclick = (e) => {
     e.preventDefault();
     authData.login = login.value;
     authData.password = password.value;
 
     if (login.value && password.value) {
-        auth(authData).then((r) => console.log(r.ok));
+        auth(authData).then((r) => {
+            if (r.ok) {
+                window.location.href = redirectPath;
+            } else {
+                alert('Данные введены неверно');
+            }
+        });
     }
 }

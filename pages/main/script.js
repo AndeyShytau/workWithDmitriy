d0d57dd7752d87905fae4ab1cf4f45e6a6dcac86
d-vooolk <volk.dmitrii@otr.ru>
2023-08-07 23:24:44 +0300

    remove redundant functional

--- pages/main/script.js
+++ pages/main/script.js
@@ -2,14 +2,11 @@ import {addNewsList} from "../../modules/addNews/news.js";
 import {addUserInfo} from "../../modules/getUserInfo/addUserInfo.js";
 import {getRequests} from "../../api/getters.js";
 import {GET_PROFILE_DATA, GET_USER_NEWS} from "../../api/constants.js";
-import {setProfileData, setUserMessagesData, setUserNewsData} from "../../api/setters.js";
-import {newUserMessages, newUserNews, newUserData} from "./constants.js";
 import {checkAuth} from "../../helpers/helpers.js";
 
 const moreInfoButton = document.getElementById('moreInfoButton');
 const newsArea = document.getElementById('newsArea');
 const sendNewsButton = document.getElementById('sendNewsButton');
-const saveProfileChanges = document.getElementById('saveProfileChanges');
 
 const newsArr = [];
 let isShowMoreInfo = false;
@@ -41,48 +38,3 @@ sendNewsButton.addEventListener("click", function () {
     newsArea.value = "";
     console.log(newsArr);
 });
-
-saveProfileChanges.onclick = () => {
-    setProfileData(newUserData).then(response => {
-        if (response.ok) {
-            response.json().then(updatedData => {
-                console.log('Обновленные данные:', updatedData);
-            }).catch(error => {
-                console.error('Ошибка при парсинге JSON:', error);
-            });
-        } else {
-            console.error('Ошибка при запросе:', response.statusText);
-        }
-    }).catch(error => {
-        console.error('Произошла ошибка:', error);
-    });
-
-    setUserNewsData(newUserNews).then(response => {
-        if (response.ok) {
-            response.json().then(updatedData => {
-                console.log('Обновленные данные:', updatedData);
-            }).catch(error => {
-                console.error('Ошибка при парсинге JSON:', error);
-            });
-        } else {
-            console.error('Ошибка при запросе:', response.statusText);
-        }
-    }).catch(error => {
-        console.error('Произошла ошибка:', error);
-    });
-
-    setUserMessagesData(newUserMessages).then(response => {
-        if (response.ok) {
-            response.json().then(updatedData => {
-                console.log('Обновленные данные:', updatedData);
-            }).catch(error => {
-                console.error('Ошибка при парсинге JSON:', error);
-            });
-        } else {
-            console.error('Ошибка при запросе:', response.statusText);
-        }
-    }).catch(error => {
-        console.error('Произошла ошибка:', error);
-    });
-}
-

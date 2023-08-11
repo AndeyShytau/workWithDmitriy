97d9106b5a8340a9c76ecf89931da4cd93bedb40
d-vooolk <volk.dmitrii@otr.ru>
2023-08-11 14:25:37 +0300

    add comments functional

--- main/constants.js
+++ main/constants.js
@@ -44,7 +44,7 @@ export const userNews = [{
     picture: 'https://media.istockphoto.com/id/1249466085/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80-%D0%B7%D0%BD%D0%B0%D1%87%D0%BE%D0%BA-%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D1%8B%D1%85-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B5%D0%B9-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0%D0%B4%D0%BF%D0%B8%D1%81%D1%8C-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8-%D0%BD%D0%B0-%D0%B3%D0%BB%D0%BE%D0%B1%D1%83%D1%81%D0%B5-%D0%BC%D1%83%D0%BB%D1%8C%D1%82%D1%8F%D1%88%D0%BD%D1%8B%D0%B9-%D1%81%D1%82%D0%B8%D0%BB%D1%8C-%D0%BD%D0%B0.jpg?s=2048x2048&w=is&k=20&c=G81o0BVYoEabvmudxaYLhT15lmUGs5POWE5B1FneXnE=',
     creationDate: date, time,
     comments: []
-},
+    },
     {
         text: 'Andrey NEW Новость 2 Каждая форма на веб-странице представлена объектом HTMLFormElement в DOM Получить доступ к форме мы можем через уже знакомые:',
         picture: 'https://media.istockphoto.com/id/1249466085/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80-%D0%B7%D0%BD%D0%B0%D1%87%D0%BE%D0%BA-%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D1%8B%D1%85-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B5%D0%B9-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0%D0%B4%D0%BF%D0%B8%D1%81%D1%8C-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8-%D0%BD%D0%B0-%D0%B3%D0%BB%D0%BE%D0%B1%D1%83%D1%81%D0%B5-%D0%BC%D1%83%D0%BB%D1%8C%D1%82%D1%8F%D1%88%D0%BD%D1%8B%D0%B9-%D1%81%D1%82%D0%B8%D0%BB%D1%8C-%D0%BD%D0%B0.jpg?s=2048x2048&w=is&k=20&c=G81o0BVYoEabvmudxaYLhT15lmUGs5POWE5B1FneXnE=',
@@ -52,12 +52,11 @@ export const userNews = [{
         comments: []
     },
     {
-        text: 'Andrey NEW Новость 2 Каждая форма на веб-странице представлена объектом HTMLFormElement в DOM Получить доступ к форме мы можем через уже знакомые:',
+        text: '123 Andrey NEW Новость 2 Каждая форма на веб-странице представлена объектом HTMLFormElement в DOM Получить доступ к форме мы можем через уже знакомые:',
         picture: 'https://media.istockphoto.com/id/1249466085/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80-%D0%B7%D0%BD%D0%B0%D1%87%D0%BE%D0%BA-%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D1%8B%D1%85-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B5%D0%B9-%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0%D0%B4%D0%BF%D0%B8%D1%81%D1%8C-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8-%D0%BD%D0%B0-%D0%B3%D0%BB%D0%BE%D0%B1%D1%83%D1%81%D0%B5-%D0%BC%D1%83%D0%BB%D1%8C%D1%82%D1%8F%D1%88%D0%BD%D1%8B%D0%B9-%D1%81%D1%82%D0%B8%D0%BB%D1%8C-%D0%BD%D0%B0.jpg?s=2048x2048&w=is&k=20&c=G81o0BVYoEabvmudxaYLhT15lmUGs5POWE5B1FneXnE=',
         creationDate: '29.07.2023',
         comments: []
     }
-
 ];
 
 export const newUserNews = [
--- modules/addNews/comments.js
+++ modules/addNews/comments.js
@@ -1,17 +1,10 @@
-import {userNews} from "../../main/constants.js";
 import {addNewsList} from "./news.js";
 
 export const sendComment = (newsArray) => {
-    const sendCommentsButton = document.getElementById('sendCommentsButton');
-    const commentsTextArea = document.getElementById('commentsTextArea');
-    const buttons = document.getElementsByClassName('sendCommentsButton');
     const wrapper = document.getElementById('newsListBlock');
     const newsElements = document.getElementsByClassName('newsListElement');
     console.log(newsElements);
 
-    // newsElements.forEach((item) => {
-    //    console.log(item);
-    //  });
     for (let key in newsElements) {
         console.log(key);
     }
@@ -20,27 +13,20 @@ export const sendComment = (newsArray) => {
 
 
     function handleClick(e) {
-
-
         if (e.target.classList.contains("sendCommentsButton")) {
+            const textValue = e.target.parentElement.previousElementSibling.textContent;
 
-            console.log(e.target.previousElementSibling.value);
+            newsArray.map((item) => {
+                if (item.text === textValue) {
+                    return {...item, comments: item.comments.push(e.target.previousElementSibling.value)}
+                }
+            });
 
-            userNews[0].comments.push(e.target.previousElementSibling.value);
-
-            console.log(userNews[0].comments)
-
-            e.target.previousElementSibling.value = "";
-
-            addNewsList(userNews, 'newsListBlock')
-
-            console.log(e.target.parentElement.parentElement)
+            addNewsList(newsArray, 'newsListBlock')
         }
 
 
     }
 
     wrapper.addEventListener('click', handleClick);
-
-
 }

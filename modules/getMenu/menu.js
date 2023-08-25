eab66223f9651530a1e0aa60c305466924db5b22
d-vooolk <volk.dmitrii@otr.ru>
2023-08-25 23:05:20 +0300

    stylized menu-bar

--- modules/getMenu/menu.js
+++ modules/getMenu/menu.js
@@ -3,12 +3,26 @@ export const getMenu = () =>{
 
     sideBar.innerHTML = `
         <ul>
-            <li><a href="../index.html">Моя страница</a></li>
-            <li><a href="">Новости</a></li>
-            <li><a href="">Мессенджер</a></li>
-            <li><a href="">Друзья</a></li>
-            <li><a href="">Сообщества</a></li>
-            <li><a href="../index1.html">Настройки</a></li>
+            <li>
+                <img src="../../icons/user.svg" alt="user">
+                <a href="../index.html">Моя страница</a>
+            </li>
+            <li>
+                <img src="../../icons/news.svg" alt="user">
+                <a href="">Новости</a>
+            </li>
+            <li>
+                <img src="../../icons/messages.svg" alt="user">
+                <a href="">Мессенджер</a>
+            </li>
+            <li>
+                <img src="../../icons/friends.svg" alt="user">
+                <a href="">Друзья</a>
+            </li>
+            <li>
+                <img src="../../icons/settings.svg" alt="user">
+                <a href="../index1.html">Настройки</a>
+            </li>
         </ul>
     `
 

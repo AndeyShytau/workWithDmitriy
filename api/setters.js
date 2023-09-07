69be2a7edd1e23380c60950dfccf03298084e3d0
d-vooolk <volk.dmitrii@otr.ru>
2023-09-07 22:07:20 +0300

    add setters.js

--- api/setters.js
+++ api/setters.js
@@ -10,7 +10,37 @@ export const setUserNewsData = (data) => {
             },
             body: JSON.stringify(data)
         }).then((resp) => resp.json())
-    }catch (error){
+    } catch (error){
         console.error(error);
     }
 }
+
+export const setProfileData = (newData) => {
+    try {
+        return fetch(SET_PROFILE_DATA,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(newData)
+            });
+    } catch (error) {
+        console.error('Произошла ошибка:', error);
+    }
+}
+
+
+export const setUserMessagesData = (newData) => {
+    try {
+        return fetch(SET_MESSAGES_DATA, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(newData)
+        });
+    } catch (error) {
+        console.error('Произошла ошибка:', error);
+    }
+};
